db38a0276fc6e64ca7b99471ba6013eff4a7480f
jhallard <john@jhallard.com>
2015-11-15 04:31:20 -0800

    bug fix regarding date parsing

--- praxyk-dot-com/app/routes/dashboard_route.py
+++ praxyk-dot-com/app/routes/dashboard_route.py
@@ -375,7 +375,8 @@ def get_params_from_url(url) :
 
 def prettify_date(date_str) :
     if not date_str : return ""
-    mydt = dt.datetime.strptime( date_str, "%Y-%m-%dT%H:%M:%S" )
+    date_str.replace("T", " ");
+    mydt = dt.datetime.strptime( date_str, "%Y-%m-%d %H:%M:%S" )
     mydt.replace(year=mydt.year-100)
     format = "%b %d %H:%M:%S %Y"
     return mydt.strftime(format)
